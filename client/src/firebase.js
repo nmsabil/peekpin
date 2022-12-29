62649de294924c0b3e0f3971428075c36b439e62
nmsabil <sabilmdtv1@gmail.com>
2022-12-29 17:07:20 +0000

    added env file for keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDgqSSyFfvmmiAsd1P78c5V0pDCPR0QNr8",
-  authDomain: "displaypin-2bd61.firebaseapp.com",
-  projectId: "displaypin-2bd61",
-  storageBucket: "displaypin-2bd61.appspot.com",
-  messagingSenderId: "557502087754",
-  appId: "1:557502087754:web:d8c4cdbcf7b65320c01cb3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSENDING_ID,
+  appId: process.env.REACT_APP_FIREBASE_API_ID,
 };
 
 const app = initializeApp(firebaseConfig);
